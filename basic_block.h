2b4cbf9947b728b53aa770d666a34eb56b5daade
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-01-24 04:14:38 +0100

    Inline custom afl_maybe_log() on x86_64

--- basic_block.h
+++ basic_block.h
@@ -5,6 +5,7 @@
 typedef struct {
     GumAddress base_address; 
     guint64 code_start, code_end;
+    uint64_t afl_prev_loc;
 } range_t;
 
 void instr_basic_block(GumStalkerIterator* iterator, GumStalkerOutput* output, gpointer user_data);
